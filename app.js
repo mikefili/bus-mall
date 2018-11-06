b7ce13171093686abe147125e1a4426aa2f2e3dd
Mike Filicetti <filicetti.michael@gmail.com>
2018-11-05 17:45:02 -0800

    Restructuring logic. Established random index function.

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
 'use strict';
 var allProducts = [];
 var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var productFP = ['assets/bag.jpg', 'assets/banana.jpg', 'assets/bathroom.jpg', 'assets/boots.jpg', 'assets/breakfast.jpg', 'assets/bubblegum.jpg', 'assets/chair.jpg', 'assets/cthulhu.jpg', 'assets/dog-duck.jpg', 'assets/dragon.jpg', 'assets/pen.jpg', 'assets/pet-sweep.jpg', 'assets/scissors.jpg', 'assets/shark.jpg', 'assets/sweep.jpg', 'assets/tauntaun.jpg', 'assets/unicorn.jpg', 'assets/usb.jpg', 'assets/water-can.jpg', 'assets/wine-glass.jpg'];
 
 function Product(name, src) {
   this.name = name;
   this.src = src;
-  this.tally = 0;
   this.votes = 0;
+  this.timesSeen = 0;
+  this.results = null;
   allProducts.push(this);
 }
-
-(function itemList() {
-  for (var i = 0; i < productNames; i++) {
-    new Product(productNames[i], './assets/' + productNames[i] + '.jpg');
-  }
-})();
-
 var tracker = {
-  // products: [],
-  totalClicks: 0,
-  leftOption: null,
-  middleOption: null,
-  rightOption: null,
-
-  mainEl: document.getElementById('main-content'),
-  leftEl: document.getElementById('leftimg'),
-  middleEl: document.getElementById('middleimg'),
-  rightEl: document.getElementById('rightimg'),
-
 
   getRandomIndex: function() {
-    return Math.floor(Math.random() * productNames.length);
+    return Math.floor(Math.random() * allProducts.length);
+
+
+
+
+
+
+
+
+
+
   },
+
   getUniqueImages: function() {
 
   },
+
   renderImages: function() {
+    var randOne = this.getRandomIndex();
+    var randTwo = this.getRandomIndex();
+    var randThree = this.getRandomIndex();
 
   },
+
   addClickTracker: function() {
 
   },
+
   clickHandler: function() {
 
   },
